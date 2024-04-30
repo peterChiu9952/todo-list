509238ab77f52052d2183de158ea41a9fe7b6e66
peterChiu <peter.chiu.9952@gmail.com>
2024-05-01 02:33:04 +0800

    Update testData

--- src/testData.js
+++ src/testData.js
@@ -5,24 +5,24 @@ export const testData = [
         id: nanoid(),
         title: "Learn React.js",
         isDone: true,
-        createdAt: new Date(2024, 4, 30, 21, 31, 0)
+        createdAt: new Date(1714127393000)
     },
     {
         id: nanoid(),
         title: "Learn Golang",
         isDone: false,
-        createdAt: new Date(2024, 4, 30, 22, 32, 0)
+        createdAt: new Date(1714300193000)
     },
     {
         id: nanoid(),
         title: "Learn Docker",
         isDone: true,
-        createdAt: new Date(2024, 4, 30, 23, 33, 0)
+        createdAt: new Date(1714386593000)
     },
     {
         id: nanoid(),
         title: "Learn something else",
         isDone: false,
-        createdAt: new Date(2024, 5, 1, 23, 34, 0)
+        createdAt: new Date(1714472993000)
     },
 ]
